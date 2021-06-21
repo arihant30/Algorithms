a6117fb6bbacc62ab1b319ae61f6a8004eacfacb
arihant30 <78214117+arihant30@users.noreply.github.com>
2021-06-22 00:44:37 +0530

    Update 00 Print alternate elements of an array .java

--- 00 Print alternate elements of an array .java
+++ 00 Print alternate elements of an array .java
@@ -34,6 +34,8 @@ class GfG
 {
     public static void print(int arr[], int n)
     {
-        // your code here
+        for(int i=0;i<arr.length;i=i+2){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
