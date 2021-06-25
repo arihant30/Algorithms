d243b3770546117912db65695a7a249eb99657f5
arihant30 <78214117+arihant30@users.noreply.github.com>
2021-06-25 18:49:07 +0530

    Create 22 Reverse a String.java

---
+++ 22 Reverse a String.java
@@ -0,0 +1,9 @@
+class Solution {
+    static String revStr(String S) {
+        StringBuilder input1 = new StringBuilder();
+        input1.append(S);
+        input1.reverse();
+
+        return (input1.toString());
+    }
+}
